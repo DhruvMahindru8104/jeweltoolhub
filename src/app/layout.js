dbc66eea7d35f6d0c1b100a06e507dc75a13873f
DhruvMahindru8104 <dhruv.mahindru08@gmail.com>
2025-08-03 18:48:39 +0530

    Update layout.js firse

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Playfair_Display } from "next/font/google";
+import "./globals.css";
 import Navbar from "@/components/mycomponents/Navbar";
 import Footer from "@/components/mycomponents/Footer";
 
@@ -14,21 +13,20 @@ const playfair = Playfair_Display({
 export const metadata = {
   title: "JewelToolHub | Goldsmith Tools & Jewellery Equipment in Amritsar",
   description: "Goldsmith tools, jewellery boxes, and accessories with direct delivery to your shop in Amritsar.",
-       verification: {
+  verification: {
     google: "NBcM0KC7LzlFvKgNFic_yLD1FHFResize2ExM6_15Mc",
   },
   other: {
     "application/ld+json": JSON.stringify({
       "@context": "https://schema.org",
-      "@type": "GoldSmithTools",
+      "@type": "JewelryStore", // Corrected type
       name: "JewelToolHub",
       image: "https://jeweltoolhub.vercel.app/Logobhai.png",
       url: "https://jeweltoolhub.vercel.app",
       telephone: "+91-6239535324",
-   
       address: {
         "@type": "PostalAddress",
-        streetAddress: "828,katra mohar singh",
+        streetAddress: "828, Katra Mohar Singh",
         addressLocality: "Amritsar",
         addressRegion: "Punjab",
         postalCode: "143001",
@@ -41,20 +39,16 @@ export const metadata = {
   },
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${playfair.className}  antialiased`}
-      >
-           <div className="max-w-7xl mx-auto px-5 sm:px-10 md:px-12 lg:px-5 space-y-16 ">
-        <Navbar />
-           </div>
+      <body className={`${playfair.className} antialiased`}>
+        <div className="max-w-7xl mx-auto px-5 sm:px-10 md:px-12 lg:px-5 space-y-16">
+          <Navbar />
+        </div>
         {children}
-        <div className="max-w-7xl mx-auto px-5 sm:px-10 md:px-12 lg:px-5 space-y-16 ">
-        <Footer/>
-
+        <div className="max-w-7xl mx-auto px-5 sm:px-10 md:px-12 lg:px-5 space-y-16">
+          <Footer />
         </div>
       </body>
     </html>
